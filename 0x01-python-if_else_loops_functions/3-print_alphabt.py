8a4070b8fdb6065b3b11e14ecb6f31faef53d9fe
Michael Amuah <mkdamuah@hotmail.com>
2022-11-30 02:06:07 +0000

    task corrections

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('e') and i != ord('q'):
-        print(f"{chr(i)}", end='')
+for c in range(ord('a'), ord('z')+1):
+    if c != (ord('q')) and c != (ord('e')):
+        print('{}'.format(chr(c)), end='')
