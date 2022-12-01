2c71cf2d5dffed3f46b041399afa959b806a8b26
Michael Amuah <mkdamuah@hotmail.com>
2022-12-01 23:44:27 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
-    if int(len(sys.argv)) == 1:
+    from sys import argv
+    if int(len(argv)) == 1:
         str_end = '.'
     else:
         str_end = ':'
 
-    if int(len(sys.argv)) != 2:
+    if int(len(argv)) != 2:
         arg_string = 'arguments'
     else:
         arg_string = 'argument'
 
-    print("{:d} {:s}{:s}".format(int(len(sys.argv)) - 1, arg_string, str_end))
+    print("{:d} {:s}{:s}".format(int(len(argv)) - 1, arg_string, str_end))
 
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
