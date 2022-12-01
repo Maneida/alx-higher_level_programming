849eb6ae52156ede240ce72d848610839fb78ef1
Michael Amuah <mkdamuah@hotmail.com>
2022-12-01 23:48:09 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -17,7 +17,7 @@
     for i in range(1, len(argv)):
         print("{:d}: {:s}".format(i, argv[i]))'''
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     from sys import argv
     userin = argv[1:]
     size = len(userin)
@@ -26,4 +26,18 @@ if __name__ == "__main__":
                  "arguments" if (size) is not 1 else "argument",
                  "." if (size) is 0 else ":"))
     for idx, arg in enumerate(userin):
-        print("{:d}: {:s}".format(idx + 1, arg))
+        print("{:d}: {:s}".format(idx + 1, arg))'''
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
