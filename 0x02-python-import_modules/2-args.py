c7b226d27c7d22b4a3cd5707baed0c2919e41d87
Michael Amuah <mkdamuah@hotmail.com>
2022-12-01 23:35:26 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    if int(len(sys.argv)) == 1:
+        str_end = '.'
+    else:
+        str_end = ':'
+
+    if int(len(sys.argv)) != 2:
+        arg_string = 'arguments'
+    else:
+        arg_string = 'argument'
+
+    print("{:d} {:s}{:s}".format(int(len(sys.argv)) - 1, arg_string, str_end))
+
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
