d9d4f9afd893bb98f98f9bcbbeee511858a3dc35
Michael Amuah <mkdamuah@hotmail.com>
2022-12-04 08:13:22 +0000

    task 1 corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if int(idx) < 0:
-        return (None)
-    elif int(idx) > len(list(my_list)):
-        return (None)
+'''def element_at(my_list, idx):
+    if int(idx) < 0 or int(idx) >= len(my_list):
+        return None
     else:
-        element = my_list[idx]
-        return (element)
+        return my_list[idx]'''
+
+
+def element_at(my_list, idx):
+    return None if int(idx) < 0 or int(idx) >= len(my_list) else my_list[idx]
 
 
 if __name__ == "__main__":
