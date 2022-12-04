bd8fd22017724e56b2f003a5de7e7304dfb1149b
Michael Amuah <mkdamuah@hotmail.com>
2022-12-04 20:48:46 +0000

    task 10 corrections

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     bool_list = []
-    for i in range(len(my_list)):
-        if my_list[i] / 2 == 0:
-            bool_list[i] = True
-        else:
-            bool_list[i] = False
+    for i in my_list:
+        bool_list.append(i % 2 == 0)
     return bool_list
 
 
