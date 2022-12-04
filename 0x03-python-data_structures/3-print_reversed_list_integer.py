1dc2c66e3f61e95f57aaa6d987c587658b48cc35
Michael Amuah <mkdamuah@hotmail.com>
2022-12-04 10:14:27 +0000

    task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    for index in range(len(my_list)):
+        if type(my_list[index]) == int:
+            print("{:d}", my_list[index])
+
+
+if __name__ == "__main__":
+    print_reversed_list_integer(my_list=[])
