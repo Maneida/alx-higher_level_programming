d030a32fc0f25942237ead4abbe1bd7dc701edc1
Michael Amuah <mkdamuah@hotmail.com>
2022-12-04 11:37:13 +0000

    task 4 and 5 corrections

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,3 +6,7 @@ def new_in_list(my_list, idx, element):
         list_copy = my_list.copy
         list_copy[idx] = element
         return list_copy
+
+
+if __name__ == "__main__":
+    new_in_list(my_list, idx, element)
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i == 'c' or 'C':
-            i = ' '
+    if my_string is not None:
+        for i in my_string:
+            if i == 'c' or 'C':
+                i = ' '
 
 
 if __name__ == "__main__":
