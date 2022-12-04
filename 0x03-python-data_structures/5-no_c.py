851d19124f880aa646c989a88f0a291eeef65b13
Michael Amuah <mkdamuah@hotmail.com>
2022-12-04 13:15:20 +0000

    task 5 corrections

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     if my_string is not None:
         for i in my_string:
-            if i == 'c' and 'C':
+            if i == 'c' or i == 'C':
                 i = ''
+            new_string += i
+        return new_string
 
 
 if __name__ == "__main__":
