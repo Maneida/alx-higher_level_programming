4832b4da1f18b34151943655cbd257bcf263df8f
Michael Amuah <mkdamuah@hotmail.com>
2022-12-04 17:36:01 +0000

    task 7 corrections

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if 0 <= len(tuple_a) < 2:
-        for i in range(2 - len(tuple_a)):
-            tuple_a += (0,)
-    elif 0 <= len(tuple_b) < 2:
-        for i in range(2 - len(tuple_b)):
-            tuple_b += (0,)
-    tuple_c = (tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1])
-    return tuple_c
+    if tuple_a is not None and tuple_b is not None:
+        if 0 <= len(tuple_a) < 2:
+            for i in range(2 - len(tuple_a)):
+                tuple_a += (0,)
+        elif 0 <= len(tuple_b) < 2:
+            for i in range(2 - len(tuple_b)):
+                tuple_b += (0,)
+        tuple_c = (tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1])
+        return tuple_c
 
 
 if __name__ == "__main__":
