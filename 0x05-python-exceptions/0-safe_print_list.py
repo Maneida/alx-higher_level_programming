609aa9d32b0808b6f6705c9f3d12bb983bcbaadc
Michael Amuah <mkdamuah@hotmail.com>
2022-12-19 21:52:49 +0000

    task 0 update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,19 +4,17 @@ def safe_print_list(my_list=[], x=0):
     try:
         list_len = 0
         for i in my_list:
-            list_len += 1 #move line 4-6 before try statement
+            list_len += 1
 
         if x <= list_len:
             pass
-        elif x > listt_len:
+        elif x > list_len:
             x = list_len
 
         for j in range(x):
-            print("{:d}".format(my_list[j], end='')
-        print("\n")
+            print("{:d}".format(my_list[j]), end='')
+        print("")
 
         return (x)
-    except:
-        if my_list == NULL:
-                  print("List is empty")
-
+    except Exception as e:
+        print(e)
