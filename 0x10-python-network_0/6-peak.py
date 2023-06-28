77436681813d8e54588ef1a610ac1d77b3daa41d
Michael Amuah <mkdamuah@hotmail.com>
2023-06-28 00:39:13 +0000

    6-corrections

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,21 +11,21 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
-    l = list_of_integers
+    l_i = list_of_integers
     beg = 0
-    end = len(l)-1
+    end = len(l_i)-1
 
-    if l[beg] > l[beg+1]:
-        return l[beg]
-    if l[end] > l[end-1]:
-        return l[end]
+    if (l_i)[beg] > (l_i)[beg+1]:
+        return (l_i)[beg]
+    if (l_i)[end] > (l_i)[end-1]:
+        return (l_i)[end]
 
     mid = (beg+end)//2
-    if l[mid-1] < l[mid] and l[mid+1] < l[mid]:
-        return l[mid]
-    if l[mid] < l[mid-1]:
-        return find_peak(l[beg:mid+1])
-    elif l[mid] < l[mid+1]:
-        return find_peak(l[mid:end+1])
+    if (l_i)[mid-1] < (l_i)[mid] and (l_i)[mid+1] < (l_i)[mid]:
+        return (l_i)[mid]
+    if (l_i)[mid] < (l_i)[mid-1]:
+        return find_peak((l_i)[beg:mid+1])
+    elif (l_i)[mid] < (l_i)[mid+1]:
+        return find_peak((l_i)[mid:end+1])
     else:
-        return l[beg]
+        return (l_i)[beg]
